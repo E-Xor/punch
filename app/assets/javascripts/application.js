83dddd821f857a2d3a5914615cc5b7f736362c91
Maksim Sundukov <e-xormail@tut.by>
2013-12-20 18:08:26 -0500

    Highlight current menu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,3 @@
 //= require lib/jquery-2.0.3.min
 //= require employee
+//= require punch
---
+++ app/assets/javascripts/punch.js
@@ -0,0 +1,14 @@
+$(function(){
+
+  var $menuSelector = $("ul.punch-nav-menu > li > a");
+  if(window.location.path == '/employees') {
+    $($menuSelector[1]).addClass('punch-active-menu');
+  }
+  else {
+    $($menuSelector[0]).addClass('punch-active-menu');
+  }
+  $menuSelector.click(function(e){
+    $menuSelector.removeClass('punch-active-menu'); // remove from all menu items
+    $(e.target).addClass('punch-active-menu');
+  });
+});
